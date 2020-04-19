c45bde2495868612c164732b678773e3b38ce719
CLEMENT <russandol@msn.com>
2020-04-19 18:38:41 +0200

    add-module-class

---
+++ Hulotte/Module.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hulotte;
+
+
+/**
+ * Class Module
+ * @author Sébastien CLEMENT<s.clement@la-taniere.net>
+ * @package Hulotte
+ */
+class Module
+{
+    /**
+     * @var string
+     */
+    const DEFINITIONS = null;
+}
